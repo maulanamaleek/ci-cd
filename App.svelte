9522131de8f9dbf707674cbd6315e20e23e1aa87
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2021-12-14 21:35:51 +0700

    hello

---
+++ App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import router from "page";
+
+  const modalWindowStyle = {
+    minWidth: "300px",
+    width: "min-content",
+  };
+
+  let page, params;
+</script>
+
+<!-- <svelte-component this={page} /> -->
+<h1>Hello World</h1>
